1c5fdca6192947cf561cc1e754351c867c7ca0f2
Jason Chen <jasonchen@Jasons-MacBook-Pro-5.local>
2019-02-24 18:03:53 -0600

    second commit

---
+++ asteroid.js
@@ -0,0 +1,9 @@
+// Run this file with: $ node asteroid.js
+// I've provided some helpful starter code.
+
+const api_key = 'ThEWyjBBPDnDKV2CVSqO4gRU3qjBlpkMJo06rwo8'
+
+const start_date = '2019-03-16';
+const end_date = '2019-03-23';
+
+const request = require('request');
